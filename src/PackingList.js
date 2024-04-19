2ee1c09037e46997ea8b15d0a32ee92588d05aff
KRISHNA MOHAN <134590019+krishnamidathana@users.noreply.github.com>
2024-04-19 16:59:07 +0530

    Update PackingList.js

--- src/PackingList.js
+++ src/PackingList.js
@@ -7,7 +7,7 @@ export default function PackingList({items ,onDeleteItem ,onToggleItems ,clearLi
 
     const [sortBy ,setSortBy] = useState('input')
    let sortedItems ;
-  
+   
    if (sortBy === 'input') sortedItems =items
    if (sortBy === 'description') sortedItems =items.slice().sort((a,b)=>a.description.localeCompare(b.description))
    if (sortBy === 'packed') sortedItems =items.slice().sort((a,b)=>Number(a.packed) -Number(b.packed))
